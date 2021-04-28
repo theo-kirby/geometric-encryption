8e16d83e4c483da9daa9b0dc6a4bb666ac98b3a6
theo-kirby <theokirby15@gmail.com>
2021-04-27 21:05:03 -0400

    Please enter the commit message for your changes. Lines starting

--- Decrypt.py
+++ Decrypt.py
@@ -1,32 +1,35 @@
 import cleantext
+import re
 
-
+alpha = " abcdefghijklmnopqrstuvwxyz"
 with open("C.txt", 'r') as f:
     
     file_ = f.read()
-    nums = cleantext.clean(file_, punct=True) 
-    print(nums)
     f.close()
 
-l = []
-t = ()
-def D(c):
-
-    p = ""
-
-    for x, y, z in c:                   # decryption function D reverses the operations of E and returns c into p
     
-        y = int(y/x)
-        
-        p += alpha[y]
-        
-    return(p)
-for index, num in enumerate(nums):
+def D(c):
+    
 
-    if index % 3 != 0:
-        t += tuple(num)    
-    else:
-        t += tuple(num)
-        l.append(t)
-        t = ()
-print(l)
+    nums = cleantext.clean(c, punct=True) 
+    p = ""
+    counter = 1
+    l = []
+    t = []
+    for x in nums.split('), ('):
+
+        if counter % 3 != 0:
+
+            t.append(x)
+            counter + 1
+        
+        elif counter % 3 == 0:
+
+            t.append(x)
+            l.append(t)
+            counter = 1
+    return(l)
+
+
+print(D(file_))
+print(file_)

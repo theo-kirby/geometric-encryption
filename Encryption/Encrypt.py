674b1a3a04af7eb67d79b9604000a7dfaa065d22
theo-kirby <theokirby15@gmail.com>
2021-06-19 13:54:40 -0400

    adding cli argument support for encrypting any files

--- Encryption/Encrypt.py
+++ Encryption/Encrypt.py
@@ -1,29 +1,33 @@
 import csv
+import sys
+import getopt
 from collections import Counter
 import math
 import cleantext
 
 alpha = " abcdefghijklmnopqrstuvwxyz"
-
-with open('p.csv',mode='r') as f:     # Read plaintext from file (p.csv)    
-
-    file_ = f.readlines()
-    words = cleantext.clean(file_, numbers=True , punct=True) 
-    f.close()
+words = ""
+inputfile = ''
+outputfile = ''
 
 def Encrypt(p): 
+    
+    c = []
+    
+    with open(inputfile, mode='r') as f:     # Read plaintext from file (p.csv)    
+        global words
+        file_ = f.readlines()
+        words = cleantext.clean(file_, numbers=True , punct=True) 
+        f.close()
   
-    c = []  
-
-    with open('c.csv', mode='w') as f_:
-
+    with open(outputfile, mode='w') as f_:
 
         fieldnames = ['x','y','z']
         writer = csv.DictWriter(f_, fieldnames=fieldnames)
         writer.writeheader()
-        print(" \n Encryption of Plaintext (p.csv) -> Cyphertext (c.csv) : \n") 
+        print(" \n Encryption of Plaintext ("+inputfile+") -> Cyphertext ("+outputfile+"+) : \n") 
  
-        for index, char in enumerate(p):   
+        for index, char in enumerate(words):   
         
             counter_ = Counter(p) 
     
@@ -37,7 +41,30 @@ def Encrypt(p):
  
     print(c)
 
-Encrypt(words)
-
-
-f.close()   
+def main(argv):
+       
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+      
+   except getopt.GetoptError:
+      print('Encrypt.py -i <inputfile> -o <outputfile>')
+      sys.exit(2)
+      
+   for opt, arg in opts:
+       
+      if opt == '-h':
+         print('Encrypt.py -i <inputfile> -o <outputfile>')
+         sys.exit()
+         
+      elif opt in ("-i", "--ifile"):
+        global inputfile
+        inputfile = arg
+         
+      elif opt in ("-o", "--ofile"):
+        global outputfile
+        outputfile = arg
+         
+if __name__ == "__main__":
+    
+   main(sys.argv[1:])
+   Encrypt(words)
